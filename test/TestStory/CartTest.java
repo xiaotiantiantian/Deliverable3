bca26324d80a730ec761e5b1ea069e73f9bedbc3
DESKTOP-8QR0K53\Zhirun Tian <tianzhirun@gmail.com>
2016-10-17 20:03:59 -0400

    Add test of login, although the website has already blocked one of my ip because too much fail attempt

---
+++ test/TestStory/CartTest.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestStory;
+
+import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test Story: test a person add/change some goods at the shopping cart
+ *
+ * As a customer I want add/remove/change things in the shopping cart So that I
+ * can checkout correctly
+ *
+ * Sceniro 1:
+ *
+ *
+ * @author Zhirun Tian
+ */
+public class CartTest extends BaseTest {
+
+    private Selenium selenium;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://store.demoqa.com/";
+        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+    }
+
+    /**
+     * Sceniro 1: Given logged in status When add something to the cart Then the
+     * user can check out correctly
+     *
+     */
+    @Test
+    public void AddItemThenCheckOutWithLoggedInStatusTest() throws Exception {
+        //log in 
+        selenium.open("/tools-qa/");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_pass", "zhangzhipeng");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_login", "zht11");
+        selenium.waitForPageToLoad("60000");
+        selenium.click("id=wp-submit");
+        selenium.waitForPageToLoad("600000"); 
+        selenium.open("/products-page/checkout/");
+        
+        selenium.waitForPageToLoad("3000000");
+        selenium.open("/products-page/product-category/ipads/apple-ipad-6-32gb-white-3d/");
+        selenium.waitForPageToLoad("3000000");
+        selenium.click("name=Buy");
+        selenium.waitForPageToLoad("3000000");
+        selenium.open("/products-page/checkout/");
+        selenium.waitForPageToLoad("300000");
+        selenium.click("css=a.step2 > span");
+        selenium.type("id=wpsc_checkout_form_2", "Zhirun");
+        selenium.type("id=wpsc_checkout_form_3", "Tian");
+        selenium.type("id=wpsc_checkout_form_4", "Upitt");
+        selenium.type("id=wpsc_checkout_form_5", "PITT");
+        selenium.type("id=wpsc_checkout_form_6", "PA");
+        selenium.select("id=wpsc_checkout_form_7", "label=USA");
+        selenium.type("id=wpsc_checkout_form_8", "15000");
+        selenium.type("id=wpsc_checkout_form_18", "1234567890");
+        selenium.type("id=wpsc_checkout_form_11", "Zhirun");
+        selenium.type("id=wpsc_checkout_form_12", "Tian");
+        selenium.type("id=wpsc_checkout_form_13", "Upitt");
+        selenium.type("id=wpsc_checkout_form_14", "PITT");
+        selenium.select("xpath=(//select[@id='wpsc_checkout_form_16_region'])[2]", "label=Pennsylvania");
+        selenium.type("id=wpsc_checkout_form_17", "11000");
+        selenium.click("css=span > input[name=\"submit\"]");
+        selenium.waitForPageToLoad("30000");
+//        selenium.click("css=a.step2 > span");
+//        selenium.waitForPageToLoad("200000");
+//        selenium.type("id=wpsc_checkout_form_11", "iuy");
+//        selenium.type("id=wpsc_checkout_form_12", "iu");
+//        selenium.type("id=wpsc_checkout_form_13", "iu");
+//        selenium.type("id=wpsc_checkout_form_14", "iu");
+//        selenium.type("id=wpsc_checkout_form_17", "999998uk");
+//        selenium.click("css=span > input[name=\"submit\"]");
+//        selenium.waitForPageToLoad("200000");
+        boolean q = selenium.isTextPresent("Fatal Error");
+        assertTrue(q);
+
+    }
+
+    /**
+     * Sceniro 1: Given logged in status When add something to the cart Then the
+     * user can check out correctly
+     *
+     */
+    @Test
+    public void AddItemThenCheckOutWithoutLoggedInStatusTest() throws Exception {
+        //log in 
+        selenium.open("/tools-qa/");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_pass", "zhangzhipeng");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_login", "zht11");
+        selenium.waitForPageToLoad("60000");
+        selenium.click("id=wp-submit");
+        selenium.waitForPageToLoad("60000");
+        selenium.open("/products-page/checkout/");
+        selenium.waitForPageToLoad("300000");
+//        selenium.click("css=a.step2 > span");
+//        selenium.waitForPageToLoad("200000");
+//        selenium.type("id=wpsc_checkout_form_11", "iuy");
+//        selenium.type("id=wpsc_checkout_form_12", "iu");
+//        selenium.type("id=wpsc_checkout_form_13", "iu");
+//        selenium.type("id=wpsc_checkout_form_14", "iu");
+//        selenium.type("id=wpsc_checkout_form_17", "999998uk");
+//        selenium.click("css=span > input[name=\"submit\"]");
+//        selenium.waitForPageToLoad("200000");
+        boolean q = selenium.isTextPresent("Checkout");
+        assertTrue(q);
+
+    }
+
+    /**
+     * Sceniro 1: Given logged in status When add something to the cart Then the
+     * user can check out correctly
+     *
+     */
+    @Test
+    public void AddItemThenRemoveAndCanNotCheckoutTest() throws Exception {
+        //log in 
+        selenium.open("/tools-qa/");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_pass", "zhangzhipeng");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_login", "zht11");
+        selenium.waitForPageToLoad("60000");
+        selenium.click("id=wp-submit");
+        selenium.waitForPageToLoad("60000");
+        selenium.open("/products-page/checkout/");
+        selenium.waitForPageToLoad("300000");
+//        selenium.click("css=a.step2 > span");
+//        selenium.waitForPageToLoad("200000");
+//        selenium.type("id=wpsc_checkout_form_11", "iuy");
+//        selenium.type("id=wpsc_checkout_form_12", "iu");
+//        selenium.type("id=wpsc_checkout_form_13", "iu");
+//        selenium.type("id=wpsc_checkout_form_14", "iu");
+//        selenium.type("id=wpsc_checkout_form_17", "999998uk");
+//        selenium.click("css=span > input[name=\"submit\"]");
+//        selenium.waitForPageToLoad("200000");
+        boolean q = selenium.isTextPresent("Checkout");
+        assertTrue(q);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+}
---
+++ test/TestStory/LoginTest.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestStory;
+
+import com.thoughtworks.selenium.Selenium;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test Story: test a person login with correct or error username and password
+ *
+ * As a customer I want to login with correct password and could not login with
+ * uncorrect password and username
+ *
+ * can checkout correctly
+ *
+ *
+ *
+ *
+ * @author Zhirun Tian
+ */
+public class LoginTest extends BaseTest {
+
+    private Selenium selenium;
+
+    @Before
+    public void setUp() throws Exception {
+        WebDriver driver = new FirefoxDriver();
+        String baseUrl = "http://store.demoqa.com/";
+        selenium = new WebDriverBackedSelenium(driver, baseUrl);
+    }
+
+    /**
+     * Sceniro 1: Given correct username and password, When input it and press
+     * login button user can login correctly
+     *
+     */
+    @Test
+    public void LoginWithWordPressPageCorrectTest() throws Exception {
+        //log in 
+        selenium.open("/tools-qa/");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_pass", "zhangzhipeng");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_login", "zht11");
+        selenium.waitForPageToLoad("60000");
+        selenium.click("id=wp-submit");
+        selenium.waitForPageToLoad("60000");
+        boolean q = selenium.isTextPresent("Howdy");
+        assertTrue(q);
+
+    }
+
+    /**
+     * Sceniro 2: 
+     * Given correct username and wrong password, 
+     * When input it and press login button user can login correctly
+     * Then the page would prompt ERROR: Invalid login credentials.
+     */
+    @Test
+    public void LoginWithWordPressPageWrongPasswordTest() throws Exception {
+        //log in 
+        selenium.open("/tools-qa/");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_pass", "zhangzhieng");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_login", "zht11");
+        selenium.waitForPageToLoad("60000");
+        selenium.click("id=wp-submit");
+        selenium.waitForPageToLoad("60000");
+
+        boolean q = selenium.isTextPresent("ERROR: Invalid login credentials.");
+        assertTrue(q);
+
+    }
+
+    /**
+     * Sceniro 3: 
+     * Given correct password and wrong username, 
+     * When input it and press login button user can login correctly
+     * Then the page would prompt ERROR: Invalid login credentials.
+     *
+     */
+    @Test
+    public void LoginWithWordPressPageWrongUserNameTest() throws Exception {
+        //log in 
+        selenium.open("/tools-qa/");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_pass", "zhangzhipeng");
+        selenium.waitForPageToLoad("60000");
+        selenium.type("id=user_login", "zhwrong");
+        selenium.waitForPageToLoad("60000");
+        selenium.click("id=wp-submit");
+        selenium.waitForPageToLoad("60000");
+        boolean q = selenium.isTextPresent("ERROR: Invalid login credentials.");
+        assertTrue(q);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
+}
